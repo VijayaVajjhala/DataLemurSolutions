8b5587ad8d86387bf3da6d854c5be0d54ab8546f
bharati-dev <vijayabharati@outlook.com>
2025-01-18 16:58:13 -0500

    duplicate comanies

---
+++ Duplicate_comanies.sql
@@ -0,0 +1,17 @@
+-- using sub query
+SELECT count(distinct company) as duplicate_companies 
+from (
+select company_id as company
+from job_listings
+group by company_id,title,description
+having count(*) > 1
+) a;
+
+-- using self join
+select count(distinct a.company_id)  as duplicate_companies
+from job_listings a
+inner join job_listings b 
+on a.company_id = b.company_id
+and a.title = b.title
+and a.description = b.description
+and a.job_id <> b.job_id
